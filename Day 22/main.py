9691a6b5a42bdc8d250eb82eb04e5ce31339c80d
juan manuel palomino <jpalominodev@gmail.com>
2024-02-29 23:49:22 -0300

    Day 22 finishing the pong game

--- Day 22/main.py
+++ Day 22/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -23,9 +25,28 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move_ball()
+
+    # detect collision with the ball
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect points for L-paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # detect points for R-paddle
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 
 screen.exitonclick()
