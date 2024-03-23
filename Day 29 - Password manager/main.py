7ef828aa0eafadecd5df2dcbc799ba13d704957c
juan manuel palomino <39222720+jpalomino92@users.noreply.github.com>
2024-03-23 16:33:55 -0300

    Pomodoro App

---
+++ Day 29 - Password manager/main.py
@@ -0,0 +1,65 @@
+import tkinter
+from tkinter import *
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("pass.txt", "a") as data_file:
+        data_file.write(f"{website} // {email} // {password} \n")
+
+    website_entry.delete(0,'end')
+    email_entry.delete(0,'end')
+    password_entry.delete(0,'end')
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50,pady=50)
+
+canvas = Canvas(width=200,height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100,image=logo_img)
+canvas.grid(column=1,row=0)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(column=0,row=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0,row=2)
+password_label = Label(text="Password:")
+password_label.grid(column=0,row=3)
+
+# Entries
+website_entry = Entry(window,width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(window,width=35)
+email_entry.grid(column=1,row=2,columnspan=2)
+email_entry.insert(0,"jpalominodev@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1,row=3)
+
+# Buttons
+add_button = Button(text="Add",width=36,command=save)
+add_button.grid(column=1,row=4,columnspan=2)
+generate_pass_button = Button(text="Generate Password")
+generate_pass_button.grid(column=2,row=3)
+
+
+
+
+
+
+
+
+
+
+canvas.mainloop()
